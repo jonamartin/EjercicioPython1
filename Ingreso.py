172818ebaba5e21ce537fc0bf7434bde2869663e
Jonathan Martin <60260218+jonamartin@users.noreply.github.com>
2020-04-14 23:35:04 -0300

    Add files via upload
    Solución propuesta ejercicio
    Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca muestre por pantalla la cadena ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario haya introducido.

---
+++ Ingreso.py
@@ -0,0 +1,7 @@
+nombre = input("Ingresa tu nombre: ")
+edad = input("Ingresa tu edad: ")
+
+print("Bienvenido, "+nombre+".\n Tienes: "+edad+" años.")
+
+#Para tomar como numero, se tiene que convertir el ingreso de datos.
+#Tener en cuenta que si se ingresa otro tipo va a haber un Error (ver handlers)
